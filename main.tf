bb1446f3b8b60adacb5a94a22ac5cbe72a552f14
Artem Lajko <artem.lajko@hpa.hamburg.de>
2024-02-07 17:23:25 +0100

    ignore microsoft_defender soc config headache

--- main.tf
+++ main.tf
@@ -30,9 +30,13 @@ resource "azurerm_kubernetes_cluster" "main" {
     }
   }
 
+
+
   lifecycle {
     ignore_changes = [
-      default_node_pool[0].node_count
+      default_node_pool[0].node_count,
+      microsoft_defender
+
     ]
   }
 
